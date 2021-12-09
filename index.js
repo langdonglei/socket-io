f5f80f4cfc2e40decbda8051f72b06e32d2e74a3
langdonglei <langdonglei@icloud.com>
2021-12-09 15:24:57 +0800

    init

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = require('express')()
 const http = require('http').createServer(app).listen(5555)
 const socket = require('socket.io').listen(http)
 // 在服务端监听的事件 需要写在connection回调函数中 在客户端直接写事件监听
-socket.on('connection', s => {
-    s.on('message', msg => {
-        s.emit('message', msg)
+socket.on('connection', socket => {
+    socket.on('message', msg => {
+        socket.emit('message', msg)
     })
 })
 
